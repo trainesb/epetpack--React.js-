ed614220b15d725a139a51bf2757c48b7094611b
trainesb <trainesben68@gmail.com>
2019-12-11 17:20:19 -0500

    SideNav

--- src/javascript/components/SideNav.jsx
+++ src/javascript/components/SideNav.jsx
@@ -1,23 +1,47 @@
 import React, {Component} from 'react';
 
-
+import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
+import Container from 'react-bootstrap/Container';
+import Image from 'react-bootstrap/Image';
 
-import '../../scss/sidenav.scss';
-
+import Logo from '../../images/CatPack_Logo.png';
 
 class SideNav extends Component {
+
     render() {
         return (
-        	<Nav defaultActiveKey="/home" className="flex-column">
-                <Nav.Link href="/shop">Shop</Nav.Link>
-                <Nav.Link href="/mission">Mission</Nav.Link>
-                <Nav.Link href="/about">About</Nav.Link>
-                <Nav.Link href="/pack-members">Pack Members</Nav.Link>
-                <Nav.Link href="/reviews">Reviews</Nav.Link>
-                <Nav.Link href="/faq">FAQ</Nav.Link>
-                <Nav.Link href="/contact">Contact</Nav.Link>
-            </Nav>
+            <Container>
+                <Navbar bg='dark' variant='dark'>
+                    <style type="text/css">
+                    {`
+                        .bg-dark {
+                            background-color: #000 !important;
+                        }
+
+                        .nav-link {
+                            color: #fff !important;
+                        }
+
+                        .nav-link:hover,
+                        .nav-link:focus {
+                            color: grey !important;
+                        }
+                    `}
+                    </style>
+                    
+                    <Nav defaultActiveKey="/home" className="flex-column">
+                        <Nav.Link href="/shop">Shop</Nav.Link>
+                        <Nav.Link href="/mission">Mission</Nav.Link>
+                        <Nav.Link href="/about">About</Nav.Link>
+                        <Nav.Link href="/pack-members">Pack Members</Nav.Link>
+                        <Nav.Link href="/reviews">Reviews</Nav.Link>
+                        <Nav.Link href="/faq">FAQ</Nav.Link>
+                        <Nav.Link href="/contact">Contact</Nav.Link>
+                    </Nav>
+
+                </Navbar>
+            </Container>
         );
     }
 }
